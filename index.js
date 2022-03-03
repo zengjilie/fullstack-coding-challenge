47fd328f394c1f2520540a247390429289d33c0a
zengjilie <zengjilie@gmail.com>
2022-03-03 14:42:59 -0600

    test api success

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const express = require('express');
-const app = express();
-const https = require('https');
+const axios = require('axios');
 
-app.set('view engine','ejs');
+require('dotenv').config();
+
+const app = express();
+
+//Set View Engine
+app.set('view engine', 'ejs');
+
+//MW
 app.use(express.json());
 
-//route
-app.get('/',async (req,res)=>{
-    res.render('index');
-    >>
+//API
+const bible = axios.create({
+    baseURL: process.env.API_URL,
+    timeout: 1000,
+    headers: {
+        'api-key': process.env.API_KEY,
+        'Accept': 'application/json'
+    },
+})
 
+//Routes
+app.get('/', async (req, res) => {
+    bible.get('')
+        .then((response) => {
+
+            const { data } = response.data;
+
+            //Get all English Bibles
+            let engBible = data.filter(entry =>{
+                if(entry.language.name == 'English'){
+                    return true;
+                }
+            });
+
+            //Send filtered data to the frontend
+            res.json(engBible);
+            // res.render('index', { bbData: JSON.stringify(engBible)});
+        })
+        .catch(err => console.log(err))
 });
 
 //port
-app.listen(5000,()=>{
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
     console.log('Listening to port 5000')
 });
